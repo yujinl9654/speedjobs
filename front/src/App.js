5469feebbc8bb2001a76d00e19c91f16cf5d81a0
Seongwon Chae <45279249+swchae516@users.noreply.github.com>
2021-04-27 15:39:31 +0900

    feat: 개인회원 탈퇴 기능 추가 (#73)
    - 회원탈퇴 페이지 추가
     - 이력서 목록 페이지 추가
    
    Co-authored-by: swchae516 <swchae516@naver.com>

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,9 @@ import RecruitmentDetail from './components/pages/RecruitmentDetail';
 import Community from './components/pages/Community';
 import PostDetail from './components/pages/PostDetail';
 import Profile from './components/pages/Profile';
+import Profile2 from './components/pages/Profile2';
 import Resume from './components/pages/Resume';
+import ResumeList from './components/pages/ResumeList';
 import RecruitLike from './components/pages/RecruitLike';
 import CommuLike from './components/pages/CommuLike';
 import MemberRegistration from './components/pages/MemberRegistration';
@@ -96,9 +98,11 @@ function App() {
           <Route path={'/recruit/detail/:id'} component={RecruitmentDetail} />
           <Route path={'/community/post/:id'} component={PostDetail} />
           <Route exact path={'/profile'} component={Profile} />
+          <Route exact path={'/profile/delete'} component={Profile2} />
           <Route exact path={'/profile/modify'} component={ProfileMd} />
           <Route exact path={'/profile/modify2'} component={ProfileMd2} />
           <Route exact path={'/resume'} component={Resume} />
+          <Route exact path={'/resume/list'} component={ResumeList} />
           <Route exact path={'/likelist/community'} component={CommuLike} />
           <Route exact path={'/likelist/recruit'} component={RecruitLike} />
           <Route exact path={'/registration'} component={MemberRegistration} />
