5eadc98b231ab86ce8f64f7a3197b2e8f8ee16cf
JANG WOOJIN <woojin7124@naver.com>
2021-04-29 14:37:48 +0900

    fix: 게시글, 공고 검색 수정 (#85)
    * fix: 스웨거 베이스 패키지 변경
    
    * fix: 파일 컨트롤러 위치 변경
    
    * fix: 게시글, 공고 쿼리문 수정

--- src/main/java/com/jobseek/speedjobs/domain/recruit/RecruitQueryRepository.java
+++ src/main/java/com/jobseek/speedjobs/domain/recruit/RecruitQueryRepository.java
@@ -1,23 +1,17 @@
 package com.jobseek.speedjobs.domain.recruit;
 
-import static com.jobseek.speedjobs.domain.company.QCompany.*;
-import static com.jobseek.speedjobs.domain.post.QPost.post;
-import static com.jobseek.speedjobs.domain.recruit.QRecruit.*;
-import static com.jobseek.speedjobs.domain.tag.QTag.*;
+import static com.jobseek.speedjobs.domain.company.QCompany.company;
+import static com.jobseek.speedjobs.domain.recruit.QRecruit.recruit;
+import static com.jobseek.speedjobs.domain.tag.QTag.tag;
 import static com.jobseek.speedjobs.utils.QueryDslUtil.getAllOrderSpecifiers;
 
-import com.jobseek.speedjobs.domain.company.QCompany;
-import com.jobseek.speedjobs.domain.tag.QTag;
 import com.jobseek.speedjobs.dto.recruit.RecruitSearchCondition;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -34,38 +28,28 @@ public class RecruitQueryRepository {
 
 	public Page<Recruit> findAll(RecruitSearchCondition condition, Pageable pageable) {
 		List<OrderSpecifier> orders = getAllOrderSpecifiers(pageable, recruit);
-		List<Recruit> content = queryFactory
-			.selectFrom(recruit)
+
+		JPAQuery<Recruit> query = queryFactory
+			.selectDistinct(recruit)
+			.from(recruit)
 			.leftJoin(recruit.tags, tag)
 			.leftJoin(recruit.company, company)
 			.where(
 				containsTagIds(condition.getTagIds()),
-				containsTitle(condition.getTitle()),
+				containsTitleOrCompanyName(condition.getTitle(), condition.getCompanyName()),
 				containsContent(condition.getContent()),
-				containsCompanyName(condition.getCompanyName()),
 				containsAddress(condition.getAddress()),
 				goeAvgSalary(condition.getAvgSalary()),
 				goeRating(condition.getRating())
-			)
+			);
+
+		List<Recruit> content = query
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
 			.orderBy(orders.toArray(new OrderSpecifier[0]))
 			.fetch().stream().distinct().collect(Collectors.toList());
 
-		JPAQuery<Recruit> countQuery = queryFactory
-			.selectFrom(recruit)
-			.leftJoin(recruit.tags, tag)
-			.leftJoin(recruit.company, company)
-			.where(
-				containsTagIds(condition.getTagIds()),
-				containsTitle(condition.getTitle()),
-				containsContent(condition.getContent()),
-				containsCompanyName(condition.getCompanyName()),
-				containsAddress(condition.getAddress()),
-				goeAvgSalary(condition.getAvgSalary()),
-				goeRating(condition.getRating())
-			);
-		return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+		return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
 	}
 
 	private BooleanExpression containsTagIds(List<Long> tagIds) {
@@ -76,14 +60,21 @@ public class RecruitQueryRepository {
 		return !StringUtils.hasText(title) ? null : recruit.title.contains(title);
 	}
 
-	private BooleanExpression containsContent(String content) {
-		return !StringUtils.hasText(content) ? null
-			: recruit.recruitDetail.content.contains(content);
-	}
-
 	private BooleanExpression containsCompanyName(String companyName) {
 		return !StringUtils.hasText(companyName) ? null
 			: recruit.company.companyName.contains(companyName);
+	}
+
+	private BooleanExpression containsTitleOrCompanyName(String title, String companyName) {
+		if (StringUtils.hasText(title) && StringUtils.hasText(companyName)) {
+			return Objects.requireNonNull(containsTitle(title)).or(containsCompanyName(companyName));
+		}
+		return StringUtils.hasText(title) ? containsTitle(title) : containsCompanyName(companyName);
+	}
+
+	private BooleanExpression containsContent(String content) {
+		return !StringUtils.hasText(content) ? null
+			: recruit.recruitDetail.content.contains(content);
 	}
 
 	private BooleanExpression containsAddress(String address) {
